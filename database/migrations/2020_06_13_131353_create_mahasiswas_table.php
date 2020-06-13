3f5568aea9064bce7d4dfd26350a15d552b88158
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-06-13 20:46:18 +0700

    create mmc mahasiswas

---
+++ database/migrations/2020_06_13_131353_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim',10)->unique();
+            $table->string('nama');
+            $table->char('jenis_kelamin',1);
+            $table->text('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
